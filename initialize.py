ae54bb5524262288cf1fd86c88783949f2d8f2b7
Amar Ramachandran <amarrmchndrn1@gmail.com>
2021-04-19 14:05:02 -0400

    apport fix

--- initialize.py
+++ initialize.py
@@ -56,6 +56,7 @@ os.system("cd ~/index; git submodule update --init")
 os.system("curl -sSO https://dl.google.com/cloudagents/add-monitoring-agent-repo.sh && sudo bash add-monitoring-agent-repo.sh && sudo apt-get update && sudo apt-get install -y stackdriver-agent && sudo service stackdriver-agent start")
 os.system("curl -sSO https://dl.google.com/cloudagents/add-logging-agent-repo.sh && sudo bash add-logging-agent-repo.sh && sudo apt-get update && sudo apt-get install google-fluentd && sudo apt-get install -y google-fluentd-catch-all-config && sudo service google-fluentd start")
 
+os.system("mkdir ~/.config")
 os.system("mkdir ~/.config/apport")
 os.system("""echo \"[main]
 	unpackaged=true > ~/.config/apport/settings""")
