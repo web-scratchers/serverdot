1cffaa2fec48a54df810222617d4422bd6205c1e
Amar Ramachandran <amarrmchndrn1@gmail.com>
2021-04-23 19:44:35 -0400

    Update initialize

--- initialize.py
+++ initialize.py
@@ -5,6 +5,7 @@ os.system("ssh-keygen -t ed25519 -C \"amarjayr@umich.edu\" -q -N \"\" -f ~/.ssh/
 os.system("ssh-keygen -t ed25519 -C \"amarjayr@umich.edu\" -q -N \"\" -f ~/.ssh/crawler")
 os.system("ssh-keygen -t ed25519 -C \"amarjayr@umich.edu\" -q -N \"\" -f ~/.ssh/set")
 os.system("ssh-keygen -t ed25519 -C \"amarjayr@umich.edu\" -q -N \"\" -f ~/.ssh/vector_string")
+os.system("ssh-keygen -t ed25519 -C \"amarjayr@umich.edu\" -q -N \"\" -f ~/.ssh/ranker")
 
 f = open("/home/boss/.ssh/config", "x")
 
@@ -19,6 +20,10 @@ Host crawler
 Host set
     Hostname github.com
     IdentityFile=~/.ssh/set
+
+Host ranker
+    Hostname github.com
+    IdentityFile=~/.ssh/ranker
 
 Host vector_string
     Hostname github.com
@@ -43,15 +48,22 @@ print("VectorString: ")
 os.system("cat ~/.ssh/vector_string.pub")
 print("\n")
 
+print("Ranker: ")
+os.system("cat ~/.ssh/ranker.pub")
+print("\n")
+
 input("Done? ")
 
 os.system("cd ~; git clone git@crawler:web-scratchers/crawler.git")
 os.system("cd ~; git clone git@index:web-scratchers/index.git")
+os.system("cd ~; git clone git@index:web-scratchers/ranker.git")
+os.system("cd ~; git clone https://github.com/web-scratchers/monitoring.git")
 os.system("cd ~/crawler; git config submodule.Vector_String.url git@vector_string:web-scratchers/Vector_String.git")
 os.system("cd ~/index; git config submodule.Vector_String.url git@vector_string:web-scratchers/Vector_String.git")
 os.system("cd ~/index; git config submodule.set.url git@set:web-scratchers/set.git")
 os.system("cd ~/crawler; git submodule update --init")
 os.system("cd ~/index; git submodule update --init")
+os.system("cd ~/ranker; git submodule update --init")
 
 os.system("curl -sSO https://dl.google.com/cloudagents/add-monitoring-agent-repo.sh && sudo bash add-monitoring-agent-repo.sh && sudo apt-get update && sudo apt-get install -y stackdriver-agent && sudo service stackdriver-agent start")
 os.system("curl -sSO https://dl.google.com/cloudagents/add-logging-agent-repo.sh && sudo bash add-logging-agent-repo.sh && sudo apt-get update && sudo apt-get install google-fluentd && sudo apt-get install -y google-fluentd-catch-all-config && sudo service google-fluentd start")
@@ -59,12 +71,8 @@ os.system("curl -sSO https://dl.google.com/cloudagents/add-logging-agent-repo.sh
 os.system("mkdir ~/.config")
 os.system("mkdir ~/.config/apport")
 os.system("touch ~/.config/apport/settings")
-os.system("""echo \"[main]
-	unpackaged=true > ~/.config/apport/settings""")
+os.system("echo -e '[main]\nunpackaged=true' > ~/.config/apport/settings")
 
 os.system("sudo sysctl -w net.ipv4.ip_local_port_range=\"15000 64000\"")
 os.system("sudo sysctl -w net.core.somaxconn=8192")
 os.system("sudo sysctl --system")
-
-os.system("pip3 install psutil")
-os.system("pip3 install flask")
