9c351c8efd7d73c56d1e4e6ed648c001c09bbf01
Amar Ramachandran <amarrmchndrn1@gmail.com>
2021-04-24 00:05:01 -0400

    Updates

--- initialize.py
+++ initialize.py
@@ -3,7 +3,6 @@ import os
 os.system("sudo apt-get install -y openssl libssl-dev build-essential gdb tmux zlib1g-dev python3-pip")
 os.system("ssh-keygen -t ed25519 -C \"amarjayr@umich.edu\" -q -N \"\" -f ~/.ssh/index")
 os.system("ssh-keygen -t ed25519 -C \"amarjayr@umich.edu\" -q -N \"\" -f ~/.ssh/crawler")
-os.system("ssh-keygen -t ed25519 -C \"amarjayr@umich.edu\" -q -N \"\" -f ~/.ssh/set")
 os.system("ssh-keygen -t ed25519 -C \"amarjayr@umich.edu\" -q -N \"\" -f ~/.ssh/vector_string")
 os.system("ssh-keygen -t ed25519 -C \"amarjayr@umich.edu\" -q -N \"\" -f ~/.ssh/ranker")
 
@@ -16,10 +15,6 @@ config = """Host index
 Host crawler
     Hostname github.com
     IdentityFile=~/.ssh/crawler
-
-Host set
-    Hostname github.com
-    IdentityFile=~/.ssh/set
 
 Host ranker
     Hostname github.com
@@ -40,10 +35,6 @@ print("Crawler: ")
 os.system("cat ~/.ssh/crawler.pub")
 print("\n")
 
-print("Set: ")
-os.system("cat ~/.ssh/set.pub")
-print("\n")
-
 print("VectorString: ")
 os.system("cat ~/.ssh/vector_string.pub")
 print("\n")
@@ -56,14 +47,15 @@ input("Done? ")
 
 os.system("cd ~; git clone git@crawler:web-scratchers/crawler.git")
 os.system("cd ~; git clone git@index:web-scratchers/index.git")
-os.system("cd ~; git clone git@index:web-scratchers/ranker.git")
+os.system("cd ~; git clone git@ranker:web-scratchers/ranker.git")
 os.system("cd ~; git clone https://github.com/web-scratchers/monitoring.git")
 os.system("cd ~/crawler; git config submodule.Vector_String.url git@vector_string:web-scratchers/Vector_String.git")
 os.system("cd ~/index; git config submodule.Vector_String.url git@vector_string:web-scratchers/Vector_String.git")
-os.system("cd ~/index; git config submodule.set.url git@set:web-scratchers/set.git")
 os.system("cd ~/crawler; git submodule update --init")
 os.system("cd ~/index; git submodule update --init")
 os.system("cd ~/ranker; git submodule update --init")
+
+os.system("cd ~/monitoring; chmod +x run.sh")
 
 os.system("curl -sSO https://dl.google.com/cloudagents/add-monitoring-agent-repo.sh && sudo bash add-monitoring-agent-repo.sh && sudo apt-get update && sudo apt-get install -y stackdriver-agent && sudo service stackdriver-agent start")
 os.system("curl -sSO https://dl.google.com/cloudagents/add-logging-agent-repo.sh && sudo bash add-logging-agent-repo.sh && sudo apt-get update && sudo apt-get install google-fluentd && sudo apt-get install -y google-fluentd-catch-all-config && sudo service google-fluentd start")
@@ -71,7 +63,7 @@ os.system("curl -sSO https://dl.google.com/cloudagents/add-logging-agent-repo.sh
 os.system("mkdir ~/.config")
 os.system("mkdir ~/.config/apport")
 os.system("touch ~/.config/apport/settings")
-os.system("echo -e '[main]\nunpackaged=true' > ~/.config/apport/settings")
+# os.system("echo -e '[main]\nunpackaged=true' > ~/.config/apport/settings")
 
 os.system("sudo sysctl -w net.ipv4.ip_local_port_range=\"15000 64000\"")
 os.system("sudo sysctl -w net.core.somaxconn=8192")
